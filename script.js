7f867e607e4824e4f122cbeda2c1d8dc65d601b9
Rabb1t228 <146752871+Rabb1t228@users.noreply.github.com>
2023-10-02 21:30:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("dataForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    // Перенаправление пользователя на указанный URL
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=qA2wFvlcpAznZajh";
+
+    // Дополнительный код для обработки данных, если необходимо
+});
+
